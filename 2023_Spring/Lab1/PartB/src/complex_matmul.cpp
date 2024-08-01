a39f7ef2e9e909bdd8fc0bc79f446ba82051c411
mxxu00 <mxxu2000@gmail.com>
2024-08-01 09:45:55 +0000

    added complex_matmul

--- 2023_Spring/Lab1/PartB/src/complex_matmul.cpp
+++ 2023_Spring/Lab1/PartB/src/complex_matmul.cpp
@@ -10,9 +10,9 @@
 #include "complex.h"
 
 void complex_matmul(
-    complex_t MatA_DRAM[MATRIX_M][MATRIX_N], 
-    complex_t MatB_DRAM[MATRIX_N][MATRIX_K], 
-    complex_t MatC_DRAM[MATRIX_M][MATRIX_K]
+    complex_t MatA_DRAM[M_M][M_N], 
+    complex_t MatB_DRAM[M_N][M_K], 
+    complex_t MatC_DRAM[M_M][M_K]
 )
 {
 #pragma HLS interface m_axi depth=1 port=MatA_DRAM offset=slave bundle=mem
@@ -21,7 +21,63 @@ void complex_matmul(
 
 #pragma HLS interface s_axilite port=return
 
-    // TODO: Insert your code here
-    // You may follow a similar style as in real_matmul.cpp    
+    complex_t MatA[M_M][M_N];
+    complex_t MatB[M_N][M_K];
+    complex_t MatC[M_M][M_K];
 
+    // Read in the data (Matrix A) from DRAM to BRAM
+    MAT_A_ROWS:
+    for(int i = 0; i < M_M; i++) {
+        MAT_A_COLS:
+        for(int j = 0; j < M_N; j++) {
+            MatA[i][j] = MatA_DRAM[i][j];
+        }
+    }
+
+    // Read in the data (Matrix B) from DRAM to BRAM
+    MAT_B_ROWS:
+    for(int i = 0; i < M_N; i++) {
+        MAT_B_COLS:
+        for(int j = 0; j < M_K; j++) {
+            MatB[i][j] = MatB_DRAM[i][j];
+        }
+    }
+
+    // Initialize product matrix C
+    MAT_C_ROWS_INIT:
+    for(int i = 0; i < M_M; i++) {
+        MAT_C_COLS_INIT:
+        for(int j = 0; j < M_K; j++) {
+            MatC[i][j].real = 0;
+            MatC[i][j].imag = 0;
+        }
+    }
+
+    // Perform complex matrix multiplication 
+    OUTER_ROWS:
+    for(int i = 0; i < M_M; i++) {
+        OUTER_COLS:
+        for(int j = 0; j < M_K; j++) {
+            INNER_ROW_COL:
+            for(int p = 0; p < M_N; p++) {
+                // Complex multiplication: (a+bi)(c+di) = (ac-bd) + (ad+bc)i
+                int_t ac = MatA[i][p].real * MatB[p][j].real;
+                int_t bd = MatA[i][p].imag * MatB[p][j].imag;
+                int_t ad = MatA[i][p].real * MatB[p][j].imag;
+                int_t bc = MatA[i][p].imag * MatB[p][j].real;
+                
+                MatC[i][j].real += ac - bd;
+                MatC[i][j].imag += ad + bc;
+            }
+        }
+    }
+
+    // Write back the data from BRAM to DRAM
+    MAT_C_ROWS:
+    for(int i = 0; i < M_M; i++) {
+        MAT_C_COLS:
+        for(int j = 0; j < M_K; j++) {
+            MatC_DRAM[i][j] = MatC[i][j];
+        }
+    }
 }
